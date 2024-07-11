e5c7a96472ce4a4f7895b68491336de4415bd21a
Aaron Ballagan <aaronbee2010@live.co.uk>
2024-07-11 21:50:04 +0100

    Refactor basket class

--- src/Basket.php
+++ src/Basket.php
@@ -3,32 +3,69 @@
 namespace App;
 
 class Basket {
+    /**
+     * This function takes an items price excluding VAT, and a boolean stating whether or not VAT must be charged with the items
+     * If the boolean is true, the function will calculate the price including VAT and return this as the sale price.
+     * If the boolean is false, the function will not add VAT to the input price and will instead return it as is.
+     */
+    private static function returnSalePrice(float $priceExVat, bool $vat): float {
+        $vatRate = 1.2;
+
+        $priceIncVat = $priceExVat * $vatRate;
+
+        return ($vat)
+            ? $priceIncVat
+            : $priceExVat;
+    }
+
+    /**
+     * The size of the separator between the name and price within a printed basket row depends on the size of the name string
+     * If the string is less than 16 characters in length, the function will return three tabs as the appropriate seperator,
+     * otherwise it will return two tabs.
+     */
+    private static function returnCorrectSeparatorForNameSize(string $name): string {
+        return (strlen($name) < 16) ? "\t\t\t" : "\t\t";
+    }
+
+    /**
+     * This function takes a name and price, fetches the appropriate separator to place between them, formats the price to
+     * 2 decimal places, then returns a basket row containing the name, separator and formatted price.
+     */
+    private static function returnPrintedBasketRow(string $name, float $price): string {
+        $separator = self::returnCorrectSeparatorForNameSize($name);     
+
+        $formattedPrice = number_format($price, 2);
+
+        return "{$name}{$separator}{$formattedPrice}\n";
+    }
+
+    /**
+     * This function takes a basket array of items, and generates a printed basket string with one row in the string for each item
+     * in the basket.
+     */
     public static function printBasket(array $basket): string {
+        // Header of printed basket string
         $basketOutput = "Item Name\t\t\tPrice\n";
-        $total = 0;
+
+        $totalPriceOfAllItemsInBasket = 0;
 
         foreach ($basket as $item) {
-            foreach ($item as $key => $value) {
-                switch ($key) {
-                    case "_name":
-                        $basketOutput .= $value;
-                        $basketOutput .= (strlen($value) < 16) ? "\t\t\t" : "\t\t";
-                        break;
-                    case "_price":
-                        $vat = $item["_vat"];
-                        $priceToAdd = ($vat) ? $value * 1.2 : $value;
-                        $basketOutput .= number_format($priceToAdd, 2) . "\n";
-                        $total += $priceToAdd;
-                        break;
-                    case "_id":
-                    case "_vat":
-                    default:
-                        break;
-                }
-            }
+            // Save item values to variables with nice names
+            $nameOfItem = $item["_name"];
+            $priceOfItemWithoutVat = $item["_price"];
+            $doesVatApplyToCurrentItem = $item["_vat"];
+
+            // Calculate sale price of item then add it to the total basket cost
+            $salePrice = self::returnSalePrice($priceOfItemWithoutVat, $doesVatApplyToCurrentItem);
+            $totalPriceOfAllItemsInBasket += $salePrice;
+
+            // Add the current basket row to the printed basket string
+            $basketOutput .= self::returnPrintedBasketRow($nameOfItem, $salePrice);
         }
 
-        $basketOutput .= "\n\t\t\tTotal\t£" . number_format($total, 2);
+        // Footer of printed basket string, with total basket cost
+        $basketOutput .= "\n\t\t\tTotal\t£" . number_format($totalPriceOfAllItemsInBasket, 2);
+
         return $basketOutput;
     }
 }
